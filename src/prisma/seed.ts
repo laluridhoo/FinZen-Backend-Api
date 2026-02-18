ccab5d11a3b42bed41b366ce3f60096473b5af40
laluridhoo <ridhobingkir@gmail.com>
2026-02-18 10:18:09 +0800

    test: implement database seeding for initial finance data

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -20,4 +20,39 @@ async function seed() {
     },
   });
   console.log(`user berhasil dibuat: ${users1.name} email: ${users1.email}`);
- // 3. Membuat Data Transaksi Yang terhubung ke User Induk
+  // 3. Membuat Data Transaksi Yang terhubung ke User Induk
+  await prisma.transaction.createMany({
+    data: [
+      {
+        userId: users1.id,
+        amount: 100,
+        type: "income",
+        category: "Gaji",
+        description: "Gaji Bulanan",
+        date: new Date("2024-01-01"),
+      },
+      {
+        userId: users1.id,
+        amount: 25000,
+        type: "expense",
+        category: "Konsumsi",
+        description: "Makan Siang",
+        date: new Date("2024-01-02"),
+      },
+    ],
+  });
+  console.log("Transaksi berhasil dibuat untuk User 1");
+  console.log("Seeding selesai");
+}
+
+//4 eksekusi fungsi seed dengan pengamananan try-catch-finally
+seed()
+  .catch((error) => {
+    //Menangani dan menangai error jika ada tipe data yang tidak sesuai atau masalah koneksi database
+    console.error("Error saat seeding data:", error);
+    process.exit(1); //Mematikan proses dengan kode error
+  })
+  .finally(async () => {
+    //wajib menghentikan koneksi Prisma Client setelah selesai untuk mencegah memory leak
+    await prisma.$disconnect();
+  });
